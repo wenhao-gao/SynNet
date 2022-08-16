d73b03e9c273595255b8b851f4fb150a05069fde
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2022-08-16 18:02:23 -0400

    refactor: consolidate data generation pre process code
    - move into a single function
    - remove hard-coded paths
    - use logging

--- src/syn_net/data_generation/make_dataset_mp.py
+++ src/syn_net/data_generation/make_dataset_mp.py
@@ -4,24 +4,47 @@ This file generates synthetic tree data in a multi-thread fashion.
 Usage:
     python make_dataset_mp.py
 """
-import numpy as np
 import multiprocessing as mp
-from time import time
 
-from syn_net.utils.data_utils import SyntheticTreeSet
-import syn_net.data_generation._mp_make as make
+import numpy as np
+from pathlib import Path
+from syn_net.data_generation.make_dataset import synthetic_tree_generator
+from syn_net.utils.data_utils import ReactionSet, SyntheticTreeSet
+from syn_net.data_generation.process_rxn_mp import _load_building_blocks # TODO: refactor
+from syn_net.config import BUILDING_BLOCKS_RAW_DIR, DATA_PREPROCESS_DIR
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def func(_x):
+    np.random.seed(_x) # dummy input to generate "unique" seed
+    tree, action = synthetic_tree_generator(building_blocks, rxns)
+    return tree, action
 
 
 if __name__ == '__main__':
 
-    pool = mp.Pool(processes=100)
+    reaction_template_id = "hb"  # "pis" or "hb" 
+    building_blocks_id = "enamine_us-2021-smiles"
+    NUM_TREES = 600_000
 
-    NUM_TREES = 600000
+    # Load building blocks
+    building_blocks_file = Path(BUILDING_BLOCKS_RAW_DIR) / f"{building_blocks_id}.csv.gz"
+    building_blocks = _load_building_blocks(building_blocks_file)
 
-    t = time()
-    results = pool.map(make.func, np.arange(NUM_TREES).tolist())
-    print('Time: ', time() - t, 's')
+    # Load genearted reactions (matched reactions <=> building blocks)
+    reactions_dir = Path(DATA_PREPROCESS_DIR)
+    reactions_file =  f"reaction-sets_{reaction_template_id}_{building_blocks_id}.json.gz"
+    r_set = ReactionSet()
+    r_set.load(reactions_dir / reactions_file)
+    rxns = r_set.rxns
 
+    # Generate synthetic trees
+    with mp.Pool(processes=64) as pool:
+        results = pool.map(func, np.arange(NUM_TREES).tolist())
+
+    # Filter out trees that were completed with action="end" 
     trees = [r[0] for r in results if r[1] == 3]
     actions = [r[1] for r in results]
 
@@ -29,10 +52,13 @@ if __name__ == '__main__':
     num_error = actions.count(-1)
     num_unfinish = NUM_TREES - num_finish - num_error
 
-    print('Total trial: ', NUM_TREES)
-    print('num of finished trees: ', num_finish)
-    print('num of unfinished tree: ', num_unfinish)
-    print('num of error processes: ', num_error)
+    logging.info(f"Total trial {NUM_TREES}")
+    logging.info(f"Number of finished trees: {num_finish}")
+    logging.info(f"Number of of unfinished tree: {num_unfinish}")
+    logging.info(f"Number of error processes: {num_error}")
 
+    # Save to local disk
     tree_set = SyntheticTreeSet(trees)
-    tree_set.save('/pool001/whgao/data/synth_net/st_pis/st_data.json.gz')
+    outfile = f"synthetic-trees_{reaction_template_id}-{building_blocks_id}.json.gz"
+    file = Path(DATA_PREPROCESS_DIR) / outfile
+    tree_set.save(file)

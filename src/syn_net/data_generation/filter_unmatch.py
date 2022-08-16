ed35aee8e1cae18588e753add44e1d6cf4864703
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2022-08-16 15:29:40 -0400

    refactor: remove hard-coded paths pre-process code

--- src/syn_net/data_generation/filter_unmatch.py
+++ src/syn_net/data_generation/filter_unmatch.py
@@ -4,22 +4,41 @@ Filters out purchasable building blocks which don't match a single template.
 from syn_net.utils.data_utils import *
 import pandas as pd
 from tqdm import tqdm
+from pathlib import Path
+from syn_net.data_generation.process_rxn_mp import _load_building_blocks # TODO: refactor
+from syn_net.config import BUILDING_BLOCKS_RAW_DIR, DATA_PREPROCESS_DIR
+import logging
 
+logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    r_path = '/pool001/whgao/data/synth_net/st_pis/reactions_pis.json.gz'
-    bb_path = '/home/whgao/scGen/synth_net/data/enamine_us.csv.gz'
+    reaction_template_id = "hb"  # "pis" or "hb" 
+    building_blocks_id = "enamine_us-2021-smiles"
+
+    # Load building blocks
+    building_blocks_file = Path(BUILDING_BLOCKS_RAW_DIR) / f"{building_blocks_id}.csv.gz"
+    building_blocks = _load_building_blocks(building_blocks_file)
+
+
+    # Load genearted reactions (matched reactions <=> building blocks)
+    reactions_dir = Path(DATA_PREPROCESS_DIR)
+    reactions_file =  f"reaction-sets_{reaction_template_id}_{building_blocks_id}.json.gz"
     r_set = ReactionSet()
-    r_set.load(r_path)
-    matched_mols = set()
+    r_set.load(reactions_dir / reactions_file)
+
+    # Identify all used building blocks (via union of sets)
+    matched_bblocks = set()
     for r in tqdm(r_set.rxns):
-        for a_list in r.available_reactants:
-            matched_mols = matched_mols | set(a_list)
+        for reactants in r.available_reactants:
+            matched_bblocks = matched_bblocks.union(set(reactants))
 
-    original_mols = pd.read_csv(bb_path, compression='gzip')['SMILES'].tolist()
 
-    print('Total building blocks number:', len(original_mols))
-    print('Matched building blocks number:', len(matched_mols))
+    logger.info(f'Total number of building blocks {len(building_blocks):d}')
+    logger.info(f'Matched number of building blocks {len(matched_bblocks):d}')
+    logger.info(f"{len(matched_bblocks)/len(building_blocks):.2%} of building blocks are applicable for the reaction template set '{reaction_template_id}'.")
 
-    df = pd.DataFrame({'SMILES': list(matched_mols)})
-    df.to_csv('/pool001/whgao/data/synth_net/st_pis/enamine_us_matched.csv.gz', compression='gzip')
+    # Save to local disk
+    df = pd.DataFrame({'SMILES': list(matched_bblocks)})
+    outfile = f"{reaction_template_id}-{building_blocks_id}-matched.csv.gz"
+    file = Path(DATA_PREPROCESS_DIR) / outfile
+    df.to_csv(file, compression='gzip')

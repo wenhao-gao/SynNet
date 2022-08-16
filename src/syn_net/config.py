00870132aa92ea671ccc851c1f2b9e9eefe7e318
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2022-08-16 13:11:53 -0400

    move paths to `config.py`

---
+++ src/syn_net/config.py
@@ -0,0 +1,8 @@
+"""Central place for all configuration, paths, and parameter."""
+
+DATA_DIR = "database"
+ASSETS_DIR = "database/assets"
+
+# 
+BUILDING_BLOCKS_RAW_DIR = f"{ASSETS_DIR}/building-blocks" 
+REACTION_TEMPLATE_DIR = f"{ASSETS_DIR}/reaction-templates" 
--- src/syn_net/data_generation/process_rxn_mp.py
+++ src/syn_net/data_generation/process_rxn_mp.py
@@ -10,24 +10,33 @@ from time import time
 
 from syn_net.utils.data_utils import Reaction, ReactionSet
 import syn_net.data_generation._mp_process as process
+from pathlib import Path
 # Silence RDKit loggers (https://github.com/rdkit/rdkit/issues/2683)
 from rdkit import RDLogger  
 RDLogger.DisableLog('rdApp.*') 
 
+from syn_net.config import REACTION_TEMPLATE_DIR, DATA_DIR
 
 if __name__ == '__main__':
-    name = 'pis'
-    path_to_rxn_templates = '/home/whgao/scGen/synth_net/data/rxn_set_' + name + '.txt'
+    name = 'hb' # "pis" or "hb"
+
+    # Load reaction templates and parse
+    path_to_rxn_templates = f'{REACTION_TEMPLATE_DIR}/{name}.txt'
     rxn_templates = []
     for line in open(path_to_rxn_templates, 'rt'):
-        rxn = Reaction(line.split('|')[1].strip())
+        template = line.split("|")[1].strip() # reaction templates are prefix with "|"
+        rxn = Reaction(template)
         rxn_templates.append(rxn)
 
+    # Filter building blocks on each reaction
     pool = mp.Pool(processes=64)
-
     t = time()
     rxns = pool.map(process.func, rxn_templates)
     print('Time: ', time() - t, 's')
 
+    # Save data to local disk
     r = ReactionSet(rxns)
-    r.save('/pool001/whgao/data/synth_net/st_pis/reactions_' + name + '.json.gz')
+    out_dir = Path(DATA_DIR) / f"pre-process/"
+    out_dir.mkdir(exist_ok=True, parents=True)
+    out_file = out_dir / f"st_{name}.json.gz"
+    r.save(out_file)

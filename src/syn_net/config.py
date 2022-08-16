7701b24f5ba28c88743ba8d6d0df7bf04edc9ecb
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2022-08-16 14:52:18 -0400

    refactor: consolidate reaction pre-process code
    - move into a single function
    - remove hard-coded paths

--- src/syn_net/config.py
+++ src/syn_net/config.py
@@ -6,3 +6,6 @@ ASSETS_DIR = "database/assets"
 # 
 BUILDING_BLOCKS_RAW_DIR = f"{ASSETS_DIR}/building-blocks" 
 REACTION_TEMPLATE_DIR = f"{ASSETS_DIR}/reaction-templates" 
+
+# Pre-processed data
+DATA_PREPROCESS_DIR = "database/pre-process"
--- src/syn_net/data_generation/process_rxn_mp.py
+++ src/syn_net/data_generation/process_rxn_mp.py
@@ -3,40 +3,62 @@ This file processes a set of reaction templates and finds applicable
 reactants from a list of purchasable building blocks.
 
 Usage:
-    python process_rxn_mp.py
+    python process__rxnmp.py
 """
 import multiprocessing as mp
+from functools import partial
+from pathlib import Path
 from time import time
 
-from syn_net.utils.data_utils import Reaction, ReactionSet
-import syn_net.data_generation._mp_process as process
-from pathlib import Path
 # Silence RDKit loggers (https://github.com/rdkit/rdkit/issues/2683)
-from rdkit import RDLogger  
-RDLogger.DisableLog('rdApp.*') 
+from rdkit import RDLogger
 
-from syn_net.config import REACTION_TEMPLATE_DIR, DATA_DIR
+from syn_net.utils.data_utils import Reaction, ReactionSet
 
-if __name__ == '__main__':
-    name = 'hb' # "pis" or "hb"
+RDLogger.DisableLog("rdApp.*")
+
+
+import pandas as pd
+
+
+def _load_building_blocks(file: Path) -> list[str]:
+    return pd.read_csv(file)["SMILES"].to_list()
+
+
+def _match_building_blocks_to_rxn(building_blocks: list[str], _rxn: Reaction):
+    _rxn.set_available_reactants(building_blocks)
+    return _rxn
+
+
+from syn_net.config import (BUILDING_BLOCKS_RAW_DIR, DATA_PREPROCESS_DIR,
+                            REACTION_TEMPLATE_DIR)
+
+if __name__ == "__main__":
+    reaction_template_id = "hb"  # "pis" or "hb"
+    building_blocks_id = "enamine_us-2021-smiles"
+
+    # Load building blocks
+    building_blocks_file = Path(BUILDING_BLOCKS_RAW_DIR) / f"{building_blocks_id}.csv.gz"
+    building_blocks = _load_building_blocks(building_blocks_file)
 
     # Load reaction templates and parse
-    path_to_rxn_templates = f'{REACTION_TEMPLATE_DIR}/{name}.txt'
-    rxn_templates = []
-    for line in open(path_to_rxn_templates, 'rt'):
-        template = line.strip() 
+    path_to__rxntemplates = Path(REACTION_TEMPLATE_DIR) / f"{reaction_template_id}.txt"
+    _rxntemplates = []
+    for line in open(path_to__rxntemplates, "rt"):
+        template = line.strip()
         rxn = Reaction(template)
-        rxn_templates.append(rxn)
+        _rxntemplates.append(rxn)
 
     # Filter building blocks on each reaction
-    pool = mp.Pool(processes=64)
     t = time()
-    rxns = pool.map(process.func, rxn_templates)
-    print('Time: ', time() - t, 's')
+    func = partial(_match_building_blocks_to_rxn, building_blocks)
+    with mp.Pool(processes=64) as pool:
+        rxns = pool.map(func, _rxntemplates)
+    print("Time: ", time() - t, "s")
 
     # Save data to local disk
     r = ReactionSet(rxns)
-    out_dir = Path(DATA_DIR) / f"pre-process/"
+    out_dir = Path(DATA_PREPROCESS_DIR)
     out_dir.mkdir(exist_ok=True, parents=True)
-    out_file = out_dir / f"st_{name}.json.gz"
+    out_file = out_dir / f"reaction-sets_{reaction_template_id}_{building_blocks_id}.json.gz"
     r.save(out_file)

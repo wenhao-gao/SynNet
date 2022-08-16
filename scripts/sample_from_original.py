84ac835d4fefb2da04620446f7798a14420df6b3
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2022-08-16 19:58:18 -0400

    refactor: remove hard-coded paths & cosmetic changes

--- scripts/sample_from_original.py
+++ scripts/sample_from_original.py
@@ -1,72 +1,75 @@
 """
 Filters the synthetic trees by the QEDs of the root molecules.
 """
-from tdc import Oracle
-qed = Oracle(name='qed')
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
-from syn_net.utils.data_utils import *
+from rdkit import Chem
+from tdc import Oracle
+from tqdm import tqdm
 
-def is_valid(smi):
-    """
-    Checks if a SMILES string is valid.
+from syn_net.config import DATA_PREPROCESS_DIR
+from syn_net.utils.data_utils import SyntheticTree, SyntheticTreeSet
 
-    Args:
-        smi (str): Molecular SMILES string.
+DATA_DIR = "pool001/whgao/data/synth_net"
+SYNTHETIC_TREES_FILE = "abc-st_data.json.gz"
 
-    Returns:
-        False or str: False if the SMILES is not valid, or the reconverted
-            SMILES string.
-    """
-    mol = Chem.MolFromSmiles(smi)
-    if mol is None:
-        return False
-    else:
-        return Chem.MolToSmiles(mol, isomericSmiles=False)
+def _is_valid_mol(mol: Chem.rdchem.Mol):
+    return mol is not None
 
 if __name__ == '__main__':
+    reaction_template_id = "hb"  # "pis" or "hb" 
+    building_blocks_id = "enamine_us-2021-smiles"
+    qed = Oracle(name='qed')
 
-    data_path = '/pool001/whgao/data/synth_net/st_pis/st_data.json.gz'
+    # Load generated synthetic trees
+    file =  Path(DATA_PREPROCESS_DIR) / f"synthetic-trees_{reaction_template_id}-{building_blocks_id}.json.gz"
     st_set = SyntheticTreeSet()
-    st_set.load(data_path)
-    data = st_set.sts
-    print(f'Finish reading, in total {len(data)} synthetic trees.')
+    st_set.load(file)
+    synthetic_trees = st_set.sts
+    print(f'Finish reading, in total {len(synthetic_trees)} synthetic trees.')
 
-    filtered_data = []
-    original_qed = []
-    qeds = []
-    generated_smiles = []
+    # Filter synthetic trees 
+    #  .. based on validity of root molecule
+    #  .. based on drug-like quality
+    filtered_data: list[SyntheticTree] = []
+    original_qed: list[float] = []
+    qeds: list[float] = []
+    generated_smiles: list[str] = []
 
     threshold = 0.5
 
-    for t in tqdm(data):
+    for t in tqdm(synthetic_trees):
         try:
-            valid_smiles = is_valid(t.root.smiles)
-            if valid_smiles:
-                if valid_smiles in generated_smiles:
-                    pass
-                else:
-                    qed_value = qed(valid_smiles)
-                    original_qed.append(qed_value)
+            smiles = t.root.smiles
+            mol = Chem.MolFromSmiles(smiles)
+            if not _is_valid_mol(mol):
+                continue
+            if smiles in generated_smiles:
+                continue
 
-                    # filter the trees based on their QEDs
-                    if qed_value > threshold or np.random.random() < (qed_value/threshold):
-                        generated_smiles.append(valid_smiles)
-                        filtered_data.append(t)
-                        qeds.append(qed_value)
-                    else:
-                        pass
-            else:
-                pass
-        except:
-            pass
+            qed_value = qed(smiles)
+            original_qed.append(qed_value)
+
+            # filter the trees based on their QEDs
+            if qed_value > threshold or np.random.random() < (qed_value/threshold):
+                generated_smiles.append(smiles)
+                filtered_data.append(t)
+                qeds.append(qed_value)
+            
+        except Exception as e:
+            print(e)
 
     print(f'Finish sampling, remaining {len(filtered_data)} synthetic trees.')
 
+    # Save to local disk
     st_set = SyntheticTreeSet(filtered_data)
-    st_set.save('/pool001/whgao/data/synth_net/st_pis/st_data_filtered.json.gz')
+    file =  Path(DATA_PREPROCESS_DIR) / f"synthetic-trees_{reaction_template_id}-{building_blocks_id}-filtered.json.gz"
+    st_set.save(file)
 
     df = pd.DataFrame({'SMILES': generated_smiles, 'qed': qeds})
-    df.to_csv('/pool001/whgao/data/synth_net/st_pis/filtered_smiles.csv.gz', compression='gzip', index=False)
+    file = Path(DATA_PREPROCESS_DIR) / f"filtered-smiles_{reaction_template_id}-{building_blocks_id}-filtered.csv.gz"
+    df.to_csv(file, compression='gzip', index=False)
 
     print('Finish!')

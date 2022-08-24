1dca7581380dad7d83c699b663ec4857f8c86785
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2022-08-24 11:32:50 -0400

    update paths in config

--- src/syn_net/config.py
+++ src/syn_net/config.py
@@ -1,18 +1,24 @@
 """Central place for all configuration, paths, and parameter."""
 
-DATA_DIR = "database"
-ASSETS_DIR = "database/assets"
+DATA_DIR = "data"
+ASSETS_DIR = "data/assets"
 
 # 
 BUILDING_BLOCKS_RAW_DIR = f"{ASSETS_DIR}/building-blocks" 
 REACTION_TEMPLATE_DIR = f"{ASSETS_DIR}/reaction-templates" 
 
 # Pre-processed data
-DATA_PREPROCESS_DIR = "database/pre-process"
-DATA_EMBEDDINGS_DIR = "database/pre-process/embeddings"
+DATA_PREPROCESS_DIR = "data/pre-process"
+DATA_EMBEDDINGS_DIR = "data/pre-process/embeddings"
 
 # Prepared data
-DATA_PREPARED_DIR = "database/prepared"
+DATA_PREPARED_DIR = "data/prepared"
 
 # Prepared data
-DATA_FEATURIZED_DIR = "database/featurized"
+DATA_FEATURIZED_DIR = "data/featurized"
+
+# Results
+DATA_RESULT_DIR = "results"
+
+# Checkpoints (& pre-trained weights)
+CHECKPOINTS_DIR = "checkpoints" # 

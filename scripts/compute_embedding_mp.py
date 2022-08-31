ad3cc0db58dcd763e0c84e12ec6bd05e1b2356c7
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2022-08-31 11:33:09 -0400

    refactor computation of embeddings

--- scripts/compute_embedding_mp.py
+++ scripts/compute_embedding_mp.py
@@ -1,16 +1,15 @@
 """
 Computes the molecular embeddings of the purchasable building blocks.
 
-The embeddings are also referred to as "output embedding". 
+The embeddings are also referred to as "output embedding".
 In the embedding space, a kNN-search will identify the 1st or 2nd reactant.
 """
 import logging
-import multiprocessing as mp
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 
+from syn_net.MolEmbedder import MolEmbedder
 from syn_net.config import DATA_EMBEDDINGS_DIR, DATA_PREPROCESS_DIR
 from syn_net.utils.predict_utils import fp_256, fp_512, fp_1024, fp_2048, fp_4096, mol_embedding, rdkit2d_embedding
 
@@ -30,37 +29,37 @@ FUNCTIONS = {
 def _load_building_blocks(file: Path) -> list[str]:
     return pd.read_csv(file)["SMILES"].to_list()
 
-def _save_embedding(file: str, embeddings: list[list[float]]):
-    embeddings = np.array(embeddings)
-
-    np.save(file, embeddings)
-    logger.info(f"Successfully saved to {file}.")
+def get_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--building-blocks-file", type=str, help="Input file with SMILES strings (First row `SMILES`, then one per line).")
+    parser.add_argument("--output-file", type=str, help="Output file for the computed embeddings.")
+    parser.add_argument("--feature", type=str, default="fp_256", choices=FUNCTIONS.keys(), help="Objective function to optimize")
+    parser.add_argument("--ncpu", type=int, default=32, help="Number of cpus")
+    # Command line args to be deprecated, only support input/output file in future.
+    parser.add_argument("--rxn-template", type=str, default="hb", choices=["hb", "pis"], help="Choose from ['hb', 'pis']")
+    parser.add_argument("--building-blocks-id", type=str, default="enamine_us-2021-smiles")
+    return parser.parse_args()
 
 if __name__ == "__main__":
 
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--feature", type=str, default="fp_256", choices=FUNCTIONS.keys(), help="Objective function to optimize")
-    parser.add_argument("--ncpu", type=int, default=64, help="Number of cpus")
-    parser.add_argument("-rxn", "--rxn-template", type=str, default="hb", choices=["hb", "pis"], help="Choose from ['hb', 'pis']")
-    parser.add_argument("--input", type=str, help="Input file with SMILES strings (One per line).")
-    args = parser.parse_args()
-
-    reaction_template_id = args.rxn_template
-    building_blocks_id = "enamine_us-2021-smiles"
+    args = get_args()
 
     # Load building blocks
-    file = Path(DATA_PREPROCESS_DIR) / f"{reaction_template_id}-{building_blocks_id}-matched.csv.gz"
-    data = _load_building_blocks(file)
+    if (file := args.building_blocks_file) is None:
+        # Try to construct filename
+        file = Path(DATA_PREPROCESS_DIR) / f"{args.rxn_template}-{args.building_blocks_id}-matched.csv.gz"
+    bblocks = _load_building_blocks(file)
     logger.info(f"Successfully read {file}.")
-    logger.info(f"Total number of building blocks: {len(data)}.")
+    logger.info(f"Total number of building blocks: {len(bblocks)}.")
 
+    # Compute embeddings
     func = FUNCTIONS[args.feature]
-    with mp.Pool(processes=args.ncpu) as pool:
-        embeddings = pool.map(func, data)
+    molembedder = MolEmbedder(processes=args.ncpu).compute_embeddings(func,bblocks)
 
-    path = Path(DATA_EMBEDDINGS_DIR)
-    path.mkdir(exist_ok=1, parents=1)
-    outfile = path / f"{reaction_template_id}-{building_blocks_id}-embeddings.npy"
-    _save_embedding(file,embeddings)
+    # Save?
+    if (outfile := args.output_file) is None:
+        # Try to construct filename
+        outfile = Path(DATA_EMBEDDINGS_DIR) / f"{args.rxn_template}-{args.building_blocks_id}-{args.feature}.npy"
+    molembedder.save_precomputed(outfile)
+

d14f88f139836c921eeaefaba9650879fe9668f0
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2022-09-26 11:55:01 -0400

    delete unused cli args, dumb-down for now

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -15,26 +15,10 @@ def get_args():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-e", "--targetembedding", type=str, default="fp", help="Choose from ['fp', 'gin']"
-    )
-    parser.add_argument(
-        "-o",
-        "--outputembedding",
+        "--input-dir",
         type=str,
-        default="fp_256",
-        help="Choose from ['fp_4096', 'fp_256', 'gin', 'rdkit2d']",
-    )
-    parser.add_argument("-r", "--radius", type=int, default=2, help="Radius for Morgan Fingerprint")
-    parser.add_argument(
-        "-b", "--nbits", type=int, default=4096, help="Number of Bits for Morgan Fingerprint"
-    )
-    parser.add_argument(
-        "-rxn",
-        "--rxn_template",
-        type=str,
-        default="hb",
-        choices=["hb", "pis"],
-        help="Choose from ['hb', 'pis']",
+        default=str(Path(DATA_FEATURIZED_DIR)) + "hb_fp_2_4096_fp_256", # TODO: dont hardcode
+        help="Input directory for the featurized synthetic trees (with {train,valid,test}-data).",
     )
     return parser.parse_args()
 
@@ -43,33 +27,13 @@ if __name__ == "__main__":
     # Parse input args
     args = get_args()
     logger.info(f"Arguments: {vars(args)}")
-    import argparse
 
+    featurized_data_dir = args.input_dir
 
-    reaction_template_id = args.rxn_template
-    embedding = args.targetembedding
-    output_emb = args.outputembedding
-
-    main_dir = (
-        Path(DATA_FEATURIZED_DIR)
-        / f"{reaction_template_id}_{embedding}_{args.radius}_{args.nbits}_{args.outputembedding}/"
-    )  # must match with dir in `st2steps.py`
-    if reaction_template_id == "hb":
-        num_rxn = 91
-    elif reaction_template_id == "pis":
-        num_rxn = 4700
-
-    # Get dimension of output embedding
-    OUTPUT_EMBEDDINGS = {
-        "gin": 300,
-        "fp_4096": 4096,
-        "fp_256": 256,
-        "rdkit2d": 200,
-    }
-    out_dim = OUTPUT_EMBEDDINGS[output_emb]
-
-    logger.info("Start splitting data.")
     # Split datasets for each MLP
-    prep_data(main_dir, num_rxn, out_dim)
+    logger.info("Start splitting data.")
+    num_rxn = 91 # Auxiliary var for indexing TODO: Dont hardcode
+    out_dim = 256 # Auxiliary var for indexing TODO: Dont hardcode
+    prep_data(featurized_data_dir, num_rxn, out_dim)
 
-    logger.info("Successfully splitted data.")
+    logger.info(f"Completed.")

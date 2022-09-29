f753a11091b8911aca85d179f11531e3217c7ec5
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2022-09-29 18:41:02 -0400

    refactor

--- scripts/search_similar.py
+++ scripts/search_similar.py
@@ -25,6 +25,8 @@ def func(fp):
     dists = np.array([DataStructs.FingerprintSimilarity(fp, fp_, metric=DataStructs.TanimotoSimilarity) for fp_ in fps_train])
     return dists.max(), dists.argmax()
 
+def _compute_fp_bitvector(smiles: list[str], radius: int=2, nbits: int=1024):
+     return [AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromSmiles(smi), radius, nBits=nbits) for smi in smiles]
 
 if __name__ == '__main__':
 
@@ -33,27 +35,28 @@ if __name__ == '__main__':
     file = '/pool001/whgao/data/synth_net/st_hb/st_train.json.gz'
     syntree_collection = SyntheticTreeSet().load(file)
     data_train = [st.root.smiles for st in syntree_collection]
-    fps_train = [AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromSmiles(smi), 2, nBits=1024) for smi in data_train]
+    fps_train = _compute_fp_bitvector(data_train)
 
     file = '/pool001/whgao/data/synth_net/st_hb/st_test.json.gz'
     syntree_collection = SyntheticTreeSet().load(file)
     data_test = [st.root.smiles for st in syntree_collection]
+    fps_test = _compute_fp_bitvector(data_test)
 
     file = '/pool001/whgao/data/synth_net/st_hb/st_valid.json.gz'
     syntree_collection = SyntheticTreeSet().load(file)
     data_valid = [st.root.smiles for st in syntree_collection]
-
-    fps_valid = [AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromSmiles(smi), 2, nBits=1024) for smi in data_valid]
-    fps_test = [AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromSmiles(smi), 2, nBits=1024) for smi in data_test]
+    fps_valid = _compute_fp_bitvector(data_valid)
 
     with mp.Pool(processes=ncpu) as pool:
         results = pool.map(func, fps_valid)
+
     similaritys = [r[0] for r in results]
     indices = [data_train[r[1]] for r in results]
     df1 = pd.DataFrame({'smiles': data_valid, 'split': 'valid', 'most similar': indices, 'similarity': similaritys})
 
     with mp.Pool(processes=ncpu) as pool:
         results = pool.map(func, fps_test)
+
     similaritys = [r[0] for r in results]
     indices = [data_train[r[1]] for r in results]
     df2 = pd.DataFrame({'smiles': data_test, 'split': 'test', 'most similar': indices, 'similarity': similaritys})

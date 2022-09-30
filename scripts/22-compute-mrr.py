98c04efa9f58212abe4fe07ae45388f030d06baf
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2022-09-30 12:51:56 -0400

    use argparse for files, clean up

--- scripts/22-compute-mrr.py
+++ scripts/22-compute-mrr.py
@@ -1,5 +1,5 @@
 """
-This function is used to compute the mean reciprocal ranking for reactant 1 
+This function is used to compute the mean reciprocal ranking for reactant 1
 selection using the different distance metrics in the k-NN search.
 """
 from syn_net.models.mlp import MLP, load_array
@@ -7,25 +7,18 @@ from scipy import sparse
 import numpy as np
 from sklearn.neighbors import BallTree
 import torch
-from syn_net.utils.predict_utils import cosine_distance, ce_distance
-
+from syn_net.encoding.distances import cosine_distance, ce_distance
 
 if __name__ == '__main__':
 
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--featurize", type=str, default='fp',
-                        help="Choose from ['fp', 'gin']")
-    parser.add_argument("-r", "--rxn_template", type=str, default='hb',
-                        help="Choose from ['hb', 'pis']")
-    parser.add_argument("--param_dir", type=str, default='hb_fp_2_4096_256',
-                        help="")
-    parser.add_argument("--radius", type=int, default=2,
-                        help="Radius for Morgan fingerprint.")
+    parser.add_argument("--ckpt-file", type=str,help="Checkpoint to load trained reactant 1 network.")
+    parser.add_argument("--embeddings-file", type=str,help="Pre-computed molecular embeddings for kNN search.")
+    parser.add_argument("--X-data-file", type=str, help="Featurized X data for network.")
+    parser.add_argument("--y-data-file", type=str, help="Featurized y data for network.")
     parser.add_argument("--nbits", type=int, default=4096,
                         help="Number of Bits for Morgan fingerprint.")
-    parser.add_argument("--out_dim", type=int, default=256,
-                        help="Output dimension.")
     parser.add_argument("--ncpu", type=int, default=8,
                         help="Number of cpus")
     parser.add_argument("--batch_size", type=int, default=64,
@@ -33,41 +26,16 @@ if __name__ == '__main__':
     parser.add_argument("--device", type=str, default="cuda:0",
                         help="")
     parser.add_argument("--distance", type=str, default="euclidean",
-                        help="Choose from ['euclidean', 'manhattan', 'chebyshev', 'cross_entropy', 'cosine']")
+                        choices=['euclidean', 'manhattan', 'chebyshev', 'cross_entropy', 'cosine'],help="Distance function for `BallTree`.")
     args = parser.parse_args()
 
-    if args.out_dim == 300:
-        validation_option = 'nn_accuracy_gin'
-    elif args.out_dim == 4096:
-        validation_option = 'nn_accuracy_fp_4096'
-    elif args.out_dim == 256:
-        validation_option = 'nn_accuracy_fp_256'
-    elif args.out_dim == 200:
-        validation_option = 'nn_accuracy_rdkit2d'
-    else:
-        raise ValueError
 
-    main_dir = '/pool001/whgao/data/synth_net/' + args.rxn_template + '_' + args.featurize + '_' + str(args.radius) + '_' + str(args.nbits) + '_' + validation_option[12:] + '/'
-    path_to_rt1 = '/home/whgao/scGen/synth_net/synth_net/params/' + args.param_dir + '/' + 'rt1.ckpt'
+    path_to_rt1 = args.ckpt_file
     batch_size = args.batch_size
     ncpu = args.ncpu
 
-    # X = sparse.load_npz(main_dir + 'X_rt1_train.npz')
-    # y = sparse.load_npz(main_dir + 'y_rt1_train.npz')
-    # X = torch.Tensor(X.A)
-    # y = torch.Tensor(y.A)
-    # _idx = np.random.choice(list(range(X.shape[0])), size=int(X.shape[0]/100), replace=False)
-    # train_data_iter = load_array((X[_idx], y[_idx]), batch_size, ncpu=ncpu, is_train=False)
-
-    # X = sparse.load_npz(main_dir + 'X_rt1_valid.npz')
-    # y = sparse.load_npz(main_dir + 'y_rt1_valid.npz')
-    # X = torch.Tensor(X.A)
-    # y = torch.Tensor(y.A)
-    # _idx = np.random.choice(list(range(X.shape[0])), size=int(X.shape[0]/10), replace=False)
-    # valid_data_iter = load_array((X[_idx], y[_idx]), batch_size, ncpu=ncpu, is_train=False)
-
-    X = sparse.load_npz(main_dir + 'X_rt1_test.npz')
-    y = sparse.load_npz(main_dir + 'y_rt1_test.npz')
+    X = sparse.load_npz(args.X_data_file)
+    y = sparse.load_npz(args.y_data_file)
     X = torch.Tensor(X.A)
     y = torch.Tensor(y.A)
     _idx = np.random.choice(list(range(X.shape[0])), size=int(X.shape[0]/10), replace=False)
@@ -76,7 +44,7 @@ if __name__ == '__main__':
 
     rt1_net = MLP.load_from_checkpoint(path_to_rt1,
                     input_dim=int(3 * args.nbits),
-                    output_dim=args.out_dim,
+                    output_dim=d,
                     hidden_dim=1200,
                     num_layers=5,
                     dropout=0.5,
@@ -90,7 +58,8 @@ if __name__ == '__main__':
     rt1_net.eval()
     rt1_net.to(args.device)
 
-    bb_emb_fp_256 = np.load('/pool001/whgao/data/synth_net/st_hb/enamine_us_emb_fp_256.npy')
+    bb_emb_fp_256 = np.load(args.embeddings_file)
+    n, d = bb_emb_fp_256.shape
 
     # for kw_metric_ in ['euclidean', 'manhattan', 'chebyshev', 'cross_entropy', 'cosine']:
     kw_metric_ = args.distance
@@ -109,12 +78,14 @@ if __name__ == '__main__':
         X, y = X.to(args.device), y.to(args.device)
         y_hat = rt1_net(X)
         dist_true, ind_true = kdtree_fp_256.query(y.detach().cpu().numpy(), k=1)
-        dist, ind = kdtree_fp_256.query(y_hat.detach().cpu().numpy(), k=bb_emb_fp_256.shape[0])
+        dist, ind = kdtree_fp_256.query(y_hat.detach().cpu().numpy(), k=n)
         ranks = ranks + [np.where(ind[i] == ind_true[i])[0][0] for i in range(len(ind_true))]
 
     ranks = np.array(ranks)
     rrs = 1 / (ranks + 1)
-    np.save('ranks_' + kw_metric_ + '.npy', ranks)
+
+    np.save('ranks_' + kw_metric_ + '.npy', ranks) # TODO: do not hard code
+
     print(f"Result using metric: {kw_metric_}")
     print(f"The mean reciprocal ranking is: {rrs.mean():.3f}")
     print(f"The Top-1 recovery rate is: {sum(ranks < 1) / len(ranks) :.3f}, {sum(ranks < 1)} / {len(ranks)}")

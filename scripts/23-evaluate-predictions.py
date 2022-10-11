ff8fd1dfa6f40f8c2f9f372990bf284249e8381f
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2022-10-11 14:39:12 -0400

    disimprove evaluation script

--- scripts/23-evaluate-predictions.py
+++ scripts/23-evaluate-predictions.py
@@ -1,15 +1,16 @@
 """Evaluate a batch of predictions on different metrics.
 The predictions are generated in `20-predict-targets.py`.
 """
+import json
+import logging
+
 import numpy as np
 import pandas as pd
 from tdc import Evaluator
 
-kl_divergence = Evaluator(name="KL_Divergence")
-fcd_distance = Evaluator(name="FCD_Distance")
-novelty = Evaluator(name="Novelty")
-validity = Evaluator(name="Validity")
-uniqueness = Evaluator(name="Uniqueness")
+from syn_net.config import MAX_PROCESSES
+
+logger = logging.getLogger(__name__)
 
 
 def get_args():
@@ -22,28 +23,35 @@ def get_args():
         type=str,
         help="Dataframe with target- and prediction smiles and similarities (*.csv.gz).",
     )
+    # Processing
+    parser.add_argument("--ncpu", type=int, default=MAX_PROCESSES, help="Number of cpus")
+    parser.add_argument("--verbose", default=False, action="store_true")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
-    args = get_args()
+    logger.info("Start.")
 
-    files = [args.file]  # TODO: not sure why the loop but let's keep it for now
+    # Parse input args
+    args = get_args()
+    logger.info(f"Arguments: {json.dumps(vars(args),indent=2)}")
 
     # Keep track of successfully and unsuccessfully recovered molecules in 2 df's
-    recovered = pd.DataFrame({"query SMILES": [], "decode SMILES": [], "similarity": []})
-    unrecovered = pd.DataFrame({"query SMILES": [], "decode SMILES": [], "similarity": []})
+    # NOTE: column names must match input dataframe...
+    recovered = pd.DataFrame({"targets": [], "decoded": [], "similarity": []})
+    unrecovered = pd.DataFrame({"targets": [], "decoded": [], "similarity": []})
 
     # load each file containing the predictions
     similarity = []
     n_recovered = 0
     n_unrecovered = 0
     n_total = 0
+    files = [args.input_file]  # TODO: not sure why the loop but let's keep it for now
     for file in files:
-        print(f"File currently being evaluated: {file}")
+        print(f"Evaluating file: {file}")
 
         result_df = pd.read_csv(file)
-        n_total += len(result_df["decode SMILES"])
+        n_total += len(result_df["decoded"])
 
         # Split smiles, discard NaNs
         is_recovered = result_df["similarity"] == 1.0
@@ -54,34 +62,7 @@ if __name__ == "__main__":
         n_unrecovered += len(unrecovered)
         similarity += unrecovered["similarity"].tolist()
 
-    # compute the following properties, using the TDC, for the succesfully recovered molecules
-    recovered_novelty_all = novelty(
-        recovered["query SMILES"].tolist(),
-        recovered["decode SMILES"].tolist(),
-    )
-    recovered_validity_decode_all = validity(recovered["decode SMILES"].tolist())
-    recovered_uniqueness_decode_all = uniqueness(recovered["decode SMILES"].tolist())
-    recovered_fcd_distance_all = fcd_distance(
-        recovered["query SMILES"].tolist(), recovered["decode SMILES"].tolist()
-    )
-    recovered_kl_divergence_all = kl_divergence(
-        recovered["query SMILES"].tolist(), recovered["decode SMILES"].tolist()
-    )
-
-    # compute the following properties, using the TDC, for the unrecovered molecules
-    unrecovered_novelty_all = novelty(
-        unrecovered["query SMILES"].tolist(), unrecovered["decode SMILES"].tolist()
-    )
-    unrecovered_validity_decode_all = validity(unrecovered["decode SMILES"].tolist())
-    unrecovered_uniqueness_decode_all = uniqueness(unrecovered["decode SMILES"].tolist())
-    unrecovered_fcd_distance_all = fcd_distance(
-        unrecovered["query SMILES"].tolist(), unrecovered["decode SMILES"].tolist()
-    )
-    unrecovered_kl_divergence_all = kl_divergence(
-        unrecovered["query SMILES"].tolist(), unrecovered["decode SMILES"].tolist()
-    )
-
-    # Print info
+    # Print general info
     print(f"N total {n_total}")
     print(f"N recovered {n_recovered} ({n_recovered/n_total:.2f})")
     print(f"N unrecovered {n_unrecovered} ({n_recovered/n_total:.2f})")
@@ -92,17 +73,21 @@ if __name__ == "__main__":
     print(f"N unfinished trees (NaN) {n_unfinished} ({n_unfinished/n_total:.2f})")
     print(f"Average similarity (unrecovered only) {np.mean(similarity)}")
 
-    print("Novelty, recovered:", recovered_novelty_all)
-    print("Novelty, unrecovered:", unrecovered_novelty_all)
+    # Evaluate on TDC evaluators
+    for metric in "KL_divergence FCD_Distance Novelty Validity Uniqueness".split():
+        evaluator = Evaluator(name=metric)
+        try:
+            score_recovered = evaluator(recovered["targets"], recovered["decoded"])
+            score_unrecovered = evaluator(unrecovered["targets"], unrecovered["decoded"])
+        except TypeError:
+            # Some evaluators only take 1 input args, try that.
+            score_recovered = evaluator(recovered["decoded"])
+            score_unrecovered = evaluator(unrecovered["decoded"])
+        except Exception as e:
+            logger.error(f"{e.__class__.__name__}: {str(e)}")
+            logger.error(e)
+            score_recovered, score_unrecovered = np.nan, np.nan
 
-    print("Validity, decode molecules, recovered:", recovered_validity_decode_all)
-    print("Validity, decode molecules, unrecovered:", unrecovered_validity_decode_all)
-
-    print("Uniqueness, decode molecules, recovered:", recovered_uniqueness_decode_all)
-    print("Uniqueness, decode molecules, unrecovered:", unrecovered_uniqueness_decode_all)
-
-    print("FCD distance, recovered:", recovered_fcd_distance_all)
-    print("FCD distance, unrecovered:", unrecovered_fcd_distance_all)
-
-    print("KL divergence, recovered:", recovered_kl_divergence_all)
-    print("KL divergence, unrecovered:", unrecovered_kl_divergence_all)
+        print(f"Evaluation metric for {evaluator.name}:")
+        print(f"    Recovered score: {score_recovered:.2f}")
+        print(f"  Unrecovered score: {score_unrecovered:.2f}")

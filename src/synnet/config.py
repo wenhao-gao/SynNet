f6287e394ad7cc0d55b1df24be9392e6e4347588
Christian Ulmer <39857842+chrulm@users.noreply.github.com>
2022-10-26 11:38:11 -0400

    delete unused constants

--- src/synnet/config.py
+++ src/synnet/config.py
@@ -9,11 +9,5 @@ MAX_PROCESSES = min(32, multiprocessing.cpu_count()) - 1
 # Pre-processed data
 DATA_PREPROCESS_DIR = "data/pre-process"
 
-# Prepared data
-DATA_FEATURIZED_DIR = "data/featurized"
-
 # Results
 DATA_RESULT_DIR = "results"
-
-# Checkpoints (& pre-trained weights)
-CHECKPOINTS_DIR = "checkpoints"
